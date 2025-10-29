e31a911040f2c65cf40476044177409857e6add9
QuirkyTurtle99 <thaddeaus1@hotmail.com>
2025-10-29 15:51:42 -0700

    pokedex work1
    trying to add pokedex to the homepage

---
+++ pokedex.js
@@ -0,0 +1,6 @@
+const pokedex = document.getElementById("pokedex");
+
+fucntion loadPokemon(){
+    //gonna find a file for the pokemon and use a loop to go through
+    //and show them on the home page.
+}
