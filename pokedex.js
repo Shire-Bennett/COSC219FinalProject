7abc4ef2cda71fb930247d086820da90263fb9c2
QuirkyTurtle99 <thaddeaus1@hotmail.com>
2025-10-29 20:43:38 -0700

    pokedex
    got the pokemon to load into the home page.

--- pokedex.js
+++ pokedex.js
@@ -1,6 +1,20 @@
 const pokedex = document.getElementById("pokedex");
 
-fucntion loadPokemon(){
-    //gonna find a file for the pokemon and use a loop to go through
-    //and show them on the home page.
+async function loadPokemon() {
+  for (let i = 1; i <= 151; i++) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+    const data = await response.json();
+
+    const card = document.createElement("div");
+    card.classList.add("pokemon-card");
+
+    card.innerHTML = `
+      <img src="${data.sprites.front_default}" alt="${data.name}">
+      <h3>#${i} ${data.name.charAt(0).toUpperCase() + data.name.slice(1)}</h3>
+    `;
+
+    pokedex.appendChild(card);
+  }
 }
+
+loadPokemon();
